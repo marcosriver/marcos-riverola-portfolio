62e7714938b9ca259aba15f2099e22e4decdb3f2
Marcos Riverola <marcosriverola@gmail.com>
2023-12-29 13:26:22 +0100

    Fix errors for build

--- components/landing-cards/profile-card.tsx
+++ components/landing-cards/profile-card.tsx
@@ -10,7 +10,7 @@ export const ProfileCard = (): React.ReactNode => {
 
   return (
         <Card className="flex flex-col items-center justify-center bg-secondary">
-            <div onMouseEnter={() => { debounce(setHovered(!hovered), 700) }} className="group h-[100px] w-[100px] md:h-[150px] md:w-[150px] xl:h-[270px] xl:w-[275px] rounded-full bg-primary border-primary border-4 [perspective:1000px]">
+            <div onMouseEnter={() => { debounce(() => { setHovered(!hovered) }, 700) }} className="group h-[100px] w-[100px] md:h-[150px] md:w-[150px] xl:h-[270px] xl:w-[275px] rounded-full bg-primary border-primary border-4 [perspective:1000px]">
                 <div className={`relative h-full w-full transition-all duration-500 ease-out [transform-style:preserve-3d] ${hovered && '[transform:rotateY(540deg)]'} `}>
                     <div className='h-full w-full absolute inset-0'>
                         <Image fill src={'/marcos-photo.png'} alt="Photo of Marcos"/>
